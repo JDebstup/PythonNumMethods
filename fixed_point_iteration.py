4ae9b6d330d52215cb8488883086d085aea90b40
Jay Piamjariyakul <j.debstup@outlook.com>
2018-07-22 23:24:18 +0700

    Update fixed_point_iteration.py
    Added additional gridlines + changed legend to match context + removed crosses for visibility to stable values

--- fixed_point_iteration.py
+++ fixed_point_iteration.py
@@ -1,7 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plot
 
-print("Fixed-Point Iteration Automation Program v 0.1")
+print("Fixed-Point Iteration Automation Program v 0.2")
 print("By Jay Piamjariyakul (github.com/JDebstup)")
 print("Compatible with Python 3 or greater")
 print("")
@@ -37,22 +37,28 @@ var_out_true_x = var_in_true_x.copy()
 #print(var_in_true_x)
 #print(var_out_true_x)
 
-plot.suptitle("Fixed-point: " + equation_input)
+plot.suptitle("Fixed-point: x = " + equation_input)
 
 plot.subplot(1, 2, 1)
 plot.title("Finding root of equation with fixed-point iteration")
-plot.plot(var_in_num, var_out_num, marker="x", color="blue", linestyle="-", label="True")
-plot.legend(("X"))
+graph_approx, = plot.plot(var_in_num, var_out_num, color="blue", linestyle="-", label="Approximation")
+plot.legend(handles=[graph_approx])
 plot.xlabel("Counter")
 plot.ylabel("Root")
+plot.minorticks_on()
+plot.grid(b=True, which='major', color='0.5', linestyle='-')
+plot.grid(b=True, which='minor', color='0.75', linestyle='--')
 
 plot.subplot(1, 2, 2)
-plot.title("Visualization of FPI")
-plot.plot(var_in_true_eq, var_out_true_eq, color="green", linestyle=":", label="True")
-plot.plot(var_in_true_x, var_out_true_x, color="red", linestyle=":", label="True")
-plot.legend((("y = " + equation_input), ("y = x")))
+plot.title("Visualization of fixed point")
+graph_true_eq, = plot.plot(var_in_true_eq, var_out_true_eq, color="green", linestyle="-", label="y = " + equation_input)
+graph_true_x, = plot.plot(var_in_true_x, var_out_true_x, color="red", linestyle="-", label="y = x")
+plot.legend(handles=[graph_true_eq, graph_true_x])
 plot.xlabel("x")
 plot.ylabel("y")
+plot.minorticks_on()
+plot.grid(b=True, which='major', color='0.5', linestyle='-')
+plot.grid(b=True, which='minor', color='0.75', linestyle='--')
 
 
 print()
